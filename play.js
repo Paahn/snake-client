72b6e707aa290e0604c75711d87586abf291324f
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-05 00:25:51 +0000

    added an improved user interaction, now we can hit ctrl^c and terminate the program

--- play.js
+++ play.js
@@ -1,3 +1,28 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
+const stdin = process.stdin;
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+// if we hit ctrl^c the program stops
+const handleUserInput = function() {
+  stdin.on('data', (key) => {
+    if (key === ('\u0003')) {
+      process.exit();
+    }
+  })
+}
+
+const setupInput = function(callback) {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  callback(stdin);
+  return stdin;
+}
+
+
 connect();
+setupInput(handleUserInput);
