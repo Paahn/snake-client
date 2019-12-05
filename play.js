3d9a22da6baf631c42a3d4e569db99b76bd8dd39
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-05 01:01:21 +0000

    changes to callback handler inside setupInput

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
-const stdin = process.stdin;
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-// if we hit ctrl^c the program stops
-const handleUserInput = function() {
-  stdin.on('data', (key) => {
-    if (key === ('\u0003')) {
-      process.exit();
-    }
-  })
-}
-
-const setupInput = function(callback) {
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  callback(stdin);
-  return stdin;
-}
-
-
 connect();
-setupInput(handleUserInput);
+
+setupInput();
+
